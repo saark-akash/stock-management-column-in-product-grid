0932408a420439932179e73a0537ba6cf2d0a85b
Akash Rajput <akash.rajput328@gmail.com>
2024-10-24 18:09:18 +0530

    Add files via upload

---
+++ Akash/StockManagementColumnInProductGrid/Ui/DataProvider/Product/AddManageStockFilterToCollection.php
@@ -0,0 +1,15 @@
+<?php
+namespace Akash\StockManagementColumnInProductGrid\Ui\DataProvider\Product;
+
+class AddManageStockFilterToCollection implements \Magento\Ui\DataProvider\AddFilterToCollectionInterface
+{
+    public function addFilter(
+        \Magento\Framework\Data\Collection $collection,
+        $field,
+        $condition = null
+    ) {
+        if (isset($condition['eq'])) {
+            $collection->addFieldToFilter($field, $condition);
+        }
+    }
+}
